b7bba9d17f2662fa6dcf09c63723bc0bc2da9c8d
andyliu <andyliu3411@gmail.com>
2026-03-17 15:26:19 -0400

    removed smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import ScrollToTop from "./components/ScrollToTop";
-import SmoothScroll from "./components/SmoothScroll";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Events from "./pages/Events";
@@ -22,39 +21,37 @@ const App = () => (
       <SpeedInsights />
       <BrowserRouter>
         <ScrollToTop />
-        <SmoothScroll smoothness={0.3}>
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            />
-            <Route
-              path="/about"
-              element={<About />}
-            />
-            <Route
-              path="/events"
-              element={<Events />}
-            />
-            <Route
-              path="/events/:eventId"
-              element={<EventDetail />}
-            />
-            <Route
-              path="/portfolio/:portfolioId"
-              element={<PortfolioDetail />}
-            />
-            <Route
-              path="/team"
-              element={<Team />}
-            />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route
-              path="*"
-              element={<NotFound />}
-            />
-          </Routes>
-        </SmoothScroll>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/events"
+            element={<Events />}
+          />
+          <Route
+            path="/events/:eventId"
+            element={<EventDetail />}
+          />
+          <Route
+            path="/portfolio/:portfolioId"
+            element={<PortfolioDetail />}
+          />
+          <Route
+            path="/team"
+            element={<Team />}
+          />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
